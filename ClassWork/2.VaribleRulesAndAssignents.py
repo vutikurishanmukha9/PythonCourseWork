d0268dbf8a3ff03cdc4af8e4131987ddae63e531
vutikurishanmukha9 <vutikurishanmukha@gmail.com>
2025-07-01 05:18:09 +0000

    01-07-2025

--- ClassWork/2.VaribleRulesAndAssignents.py
+++ ClassWork/2.VaribleRulesAndAssignents.py
@@ -1,14 +1,14 @@
 myvar=10 ##Valid Assignment
 mYvar=10 ##Valid Assignment
 My_var=10 ##Valid Assignment
-my@var=10 ##SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?
+# my@var=10 ##SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?
 _myvar=10 ##Valid Assignment
-@myvar=10 ##SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
-my var = 10 ##SyntaxError: invalid syntax
+# @myvar=10 ##SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+# my var = 10 ##SyntaxError: invalid syntax
 myvar=10 #case senstive
 Myvar=20 #case senstive
 a = 10 #storing the values into varibles
 b = 20 #Stroing the values into varibles
-10 20 #Output before swaping
+# 10 20 #Output before swaping
 a,b = b,a #Swaping operation is being performed
-20 10 #Output after swaping the varibles
+# 20 10 #Output after swaping the varibles
