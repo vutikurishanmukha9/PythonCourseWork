ad72cf8ddd5b874a4b811e0a52bb3afec85bf2ab
vutikurishanmukha9 <vutikurishanmukha@gmail.com>
2025-07-02 06:12:45 +0000

    02/07/2025

--- ClassWork/4.PythonOperators.py
+++ ClassWork/4.PythonOperators.py
@@ -3,13 +3,23 @@
 #Arithmetic Operators
 a = 20
 b = 10
-print("Addition(+):",a+b) #30 //Addition
-print("Subtraction(-):",a-b) #10 //Subtraction
-print("Multiplication(*):",a*b) #200 //Multiplication
-print("Floor Division(//):",a//b) #2 //Floor divison to get the output in Integer
-print("Divison(/):",a/b) #2.0 //Division to print the remainder 
-print("Modulos Divison(%):",a%b) #0 //Modulos Divison to print remainder
-print("Exponentional(**):",a**b) #10240000000000 //Raises the first number to the power of the second
+print("Addition(+) of a,b:",a+b) #30 //Addition
+print("Subtraction(-) of a,b:",a-b) #10 //Subtraction
+print("Multiplication(*) of a,b:",a*b) #200 //Multiplication
+print("Floor Division(//) of a,b:",a//b) #2 //Floor divison to get the output in Integer
+print("Divison(/) of a,b:",a/b) #2.0 //Division to print the remainder 
+print("Modulos Divison(%) of a,b:",a%b) #0 //Modulos Divison to print remainder
+print("Exponentional(**)of a,b:",a**b) #10240000000000 //Raises the first number to the power of the second
 
 
-#Comparison Operators
+# Comparison Operators
+
+print("a:", a)  # Output: 20
+print("b:", b)  # Output: 10
+
+print("Equals to (==):", a == b)               # False
+print("Not Equals to (!=):", a != b)           # True
+print("Greater than (>):", a > b)              # True
+print("Greater than or Equal to (>=):", a >= b)  # True
+print("Less than (<):", a < b)                 # False
+print("Less than or Equal to (<=):", a <= b)   # False
