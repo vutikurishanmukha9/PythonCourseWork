67089922b771062d43433641fdd0c6091edf9dba
vutikurishanmukha9 <vutikurishanmukha@gmail.com>
2025-07-02 05:47:19 +0000

    Python Airthmetic Operators

---
+++ ClassWork/4.PythonOperators.py
@@ -0,0 +1,12 @@
+#4.PythonOperators
+
+#Arithmetic Operators
+a = 20
+b = 10
+print("Addition(+):",a+b) #30 //Addition
+print("Subtraction(-):",a-b) #10 //Subtraction
+print("Multiplication(*):",a*b) #200 //Multiplication
+print("Floor Division(//):",a//b) #2 //Floor divison to get the output in Integer
+print("Divison(/):",a/b) #2.0 //Division to print the remainder 
+print("Modulos Divison(%):",a%b) #0 //Modulos Divison to print remainder
+print("Exponentional(**):",a**b) #10240000000000 //Raises the first number to the power of the second
