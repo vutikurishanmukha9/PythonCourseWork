197f22f9d44c6116c74167f344075eceaa8bb8e8
vutikurishanmukha9 <vutikurishanmukha@gmail.com>
2025-07-02 06:50:14 +0000

    02/07/20250

--- ClassWork/4.PythonOperators.py
+++ ClassWork/4.PythonOperators.py
@@ -3,9 +3,11 @@
 #Arithmetic Operators
 a = 20
 b = 10
+c = 30
 
 print("a:", a)  # Output: 20
 print("b:", b)  # Output: 10
+print("c:", c)  # Output 30
 
 print("Addition(+) of a,b:",a+b) #30 //Addition
 print("Subtraction(-) of a,b:",a-b) #10 //Subtraction
@@ -27,3 +29,22 @@ print("Greater than (>):", a > b)              # True
 print("Greater than or Equal to (>=):", a >= b)  # True
 print("Less than (<):", a < b)                 # False
 print("Less than or Equal to (<=):", a <= b)   # False
+
+#Assignment Operators
+print("Assiging a value to a:", a)  #a is assigned to 20
+a += b
+print("Add & Assign to a:",a)  # a is Added & Assigned to b
+a -= b
+print("Subtract & Assign to a:",a) #a Subtracted & Assigned to b
+a *= b
+print("Multiply & Assign to a:",a) #a is Multipled & Assigned to a b
+a /= b
+print("Divide & Assign to a:",a) #b is Divided & Assigned to a
+a //= b
+print("Floor Divide & Assign to a:",a) #b is Floor Divison & Assigned to a
+a %= b
+print("Modulus & Assign to a:",a) # b is Modulus & Assign to a
+a **= b
+print("Exponentiate & Assign to a:",a) #b is Exponentiate & Assign to a
+
+# Logical Operators
