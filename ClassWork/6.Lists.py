282187b1af882c5be98c17cbd92d54061eb3409a
vutikurishanmukha9 <vutikurishanmukha@gmail.com>
2025-07-05 05:34:06 +0000

    Lists

---
+++ ClassWork/6.Lists.py
@@ -0,0 +1,7 @@
+# 1. Basic Features of Lists
+# ● Ordered: Lists maintain the order of elements.
+# ● Mutable: Elements can be modified after creation.
+# ● Indexed: Elements can be accessed using an index (starting from 0).
+# ● Allow Duplicates: Lists can contain duplicate values.
+# ● Heterogeneous: Lists can contain different data types (e.g., int, str, float, etc.).
+# ● Dynamic: Size is not fixed; elements can be added or removed dynamically.
