e89c3e4ed62ae47328550013819d45aca7f31dd5
vutikurishanmukha9 <vutikurishanmukha@gmail.com>
2025-07-07 06:13:27 +0000

    list operations

---
+++ ClassWork/7.1Tuples.py
@@ -0,0 +1,9 @@
+# 1. Introduction to Tuples
+# ● A tuple is an immutable, ordered collection of elements.
+# ● Tuples are similar to lists, but once a tuple is created, its contents cannot be changed.
+# ● Tuples can store any type of data (int, float, string, list, etc.).
+# ● Tuples are faster than lists because they are immutable.
+
+t = () #Decalring the tuple
+t = tuple() #Declaring the tuple using the constructor
+t = (1,2.3,4+3j,'string',[1,2,3,4,5,6],(1,2,3,4,5,6),True, {1,2,3,4,5,6},{'name': 'xyz'}) #storing the elememts in the tuple
--- ClassWork/7.Tuples.py
+++
@@ -1,5 +0,0 @@
-# 1. Introduction to Tuples
-# ● A tuple is an immutable, ordered collection of elements.
-# ● Tuples are similar to lists, but once a tuple is created, its contents cannot be changed.
-# ● Tuples can store any type of data (int, float, string, list, etc.).
-# ● Tuples are faster than lists because they are immutable.
