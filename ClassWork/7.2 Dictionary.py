f39963953481f32f81c6b5fb05b812d1cae031cd
vutikurishanmukha9 <vutikurishanmukha@gmail.com>
2025-07-07 06:21:30 +0000

    Dictionary 07/07/2025

---
+++ ClassWork/7.2 Dictionary.py
@@ -0,0 +1,6 @@
+# *Dictionary*
+# A dictionary in Python is an ordered, mutable collection that stores key-value pairs.
+# Unlike lists or tuples, which are indexed by position, dictionaries are indexed by unique keys.
+
+# 1. Introduction to Dictionary
+# A dictionary is defined using curly braces {}, where each key is followed by a colon :, and the key-value pairs are separated by commas.
