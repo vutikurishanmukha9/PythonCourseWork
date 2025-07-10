62f4f4ee8f4906b7a4e621644ebe71ea5b77251d
vutikurishanmukha9 <vutikurishanmukha@gmail.com>
2025-07-10 11:25:00 +0000

    Conditonal Statements

--- ClassWork/12.Conditional Statements.py
+++ ClassWork/12.Conditional Statements.py
@@ -74,6 +74,7 @@ else:
 '''Weekend Budget Plan'''
 
 amount = int(input("Enter the Amount that you want to spend on weekend:"))
+
 if amount > 20000:
     print("Go to Goa")
 elif amount > 15000:
@@ -90,3 +91,42 @@ elif amount > 100:
     print("Go for the street food")
 else:
     print("Wait for Your Time")
+
+
+'''Example for If, Elif and Else'''
+'''Grades Based On Your marks'''
+#O grade = above 90 Marks
+#A+ grade = 90 marks - 85 marks
+#A grade = 84 marks - 71 marks
+#B grade = 70 marks - 61 marks
+#C grade = 60 marks - 51 marks
+#D grade = 50 marks - 41 marks
+#E grade = 40 marks - 35 marks
+#F grade = Below 35 marks
+
+data = {
+    1:{'name':'kohli','attempt_status':False,'Python':0,'sql':0,'powerBI':0},
+    2:{'name':'Bhuvi','attempt_status':True,'Python':100,'sql':90,'powerBI':80},
+    3:{'name':'Rajat','attempt_status':True,'Python':70,'sql':90,'powerBI':50},
+    4:{'name':'Jitesh','attempt_status':False,'Python':30,'sql':100,'powerBI':25},
+    5:{'name':'Salt','attempt_status':True,'Python':60,'sql':40,'powerBI':35}
+}
+
+stuid = int(input("Enter the Student ID:"))
+print(data[stuid])
+if data[stuid]['attempt_status']:
+    total = (data[stuid]['Python'] + data[stuid]['sql'] + data[stuid]['powerBI']) / 3
+    if total >= 90:
+        print(f"Congrats !!!!!\n{data[stuid]['name']} got 'A' grade")
+        print("Feedback: Good and keep it up!")
+    elif total >= 75:
+        print(f"Congrats !!!!!\n{data[stuid]['name']} got 'B' grade")
+        print("Feedback: Good and work more!")
+    elif total >= 50:
+        print(f"Congrats !!!!!\n{data[stuid]['name']} got 'C' grade")
+        print("Feedback: Can work hard to achieve more!")
+    else:
+        print(f"Congrats !!!!!\n{data[stuid]['name']} got 'D' grade")
+        print("Feedback: You have to work hard!")
+else:
+    print("Student has not attempted the test.")
