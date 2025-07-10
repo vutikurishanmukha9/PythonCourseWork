315a138d7304c9270828fe9127f3814fdba680f6
vutikurishanmukha9 <vutikurishanmukha@gmail.com>
2025-07-10 06:07:57 +0000

    Conditional statements

---
+++ ClassWork/12.Conditional Statements.py
@@ -0,0 +1,49 @@
+# Conditional Statements
+# Conditional statements allow a program to execute certain pieces of code based on whether
+# a specific condition is True or False. They help in decision-making processes within the code.
+# In Python, the main types of conditional statements are:
+# 1. if Statement
+# 2. if-else Statement
+# 3. if-elif-else Statement
+# 4. Nested Conditional Statements
+
+
+# 1. if Statement
+# The if statement is the most basic conditional statement. It checks if a condition is True. If it # is, the block of code
+# under the if statement is executed. If the condition is False, the code is skipped.
+
+#syntax
+# '''simple if'''
+# if condition:
+#     #if-True-statements
+# '''if-else'''
+# if condition:
+#     #if-true-statements
+# else:
+#     #False-statements
+
+# '''if-elif-else'''
+# if condition:
+#     #if-true-statements
+# elif condition:
+#     #cond1 statements
+# elif condition:
+#     #Cond2 statements
+# .
+# .
+# .
+# .
+# .
+# elif condition:
+#     #Cond N Statement
+# else:
+#     #False-statements
+
+# '''nested if'''
+# if condition-outer:
+#     if condition -inner:
+#         #if0True stmts
+#     else:
+#         #if-false stmts
+# else:
+#     #False stmts
