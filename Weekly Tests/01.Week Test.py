53971719f2df2e31ffd37fa5ebe7c12df2bb1fcc
vutikurishanmukha9 <vutikurishanmukha@gmail.com>
2025-07-15 14:28:21 +0000

    week test

--- Weekly Tests/01.Week Test.py
+++ Weekly Tests/01.Week Test.py
@@ -31,4 +31,63 @@ s = s.replace('u', '*')
 
 print(s)
 
+#Shopping cart Analyzer
+#input- list of floats seperated by space 
+#Output - Two lines
+
+n = list(map(float, input("Enter the Numbers: ").split()))
+print("Sum:", sum(n))
+print("Max:", max(n))
+
+
+#Secure login system (Tuple and Conditional)
+
+#input - Two strings: username and password
+#Output - Login Successful / Acess Denied
+
+credentials = {"admin" , "python123"}
+
+n = input("Enter the username: ")
+s = input("Enter the Password: ")
+
+if n and s in credentials:
+    print("Login Successful")
+else:
+    print("Acess Denied")
+
+
+#Clean my list (List and Type Conversions)
+#Input - String of numbers of names seperated by commas
+#Output - Print a sorted list (alphabetical) of unique
+n = input("Enter names or numbers separated by commas: ").split(",")
+unique_items = set(n)              
+sorted_list = sorted(unique_items) 
+print(list(sorted_list))         
+
+#Student Marks Record (Dictonary Operations)
+#input - First line:integer n 
+#Next n lines:namesand marks (sepearted by spaces)
+#Output - Name of the student with highest marks
+
+n = int(input("Enter number of students: "))
+student_marks = {}
+for _ in range(n):
+    name, marks = input("Enter name and marks: ").split()
+    student_marks[name] = int(marks)
+topper = max(student_marks, key=student_marks.get)
+print("Topper:", topper)
+
+#Reverse My Words
+#Input - a string sentence
+# output - Each word reversed, seperated by space
+
+sentence = input("Enter a sentence: ")
+words = sentence.split()
+reversed_word_list = []
+for word in words:
+    reversed_word = word[::-1]    
+    reversed_word_list.append(reversed_word)  
+result = ' '.join(reversed_word_list)
+print("Reversed sentence word by word:", result)
+
 
