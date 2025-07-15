e1ce5b18a7054307926f0030b38c1f17048c01d5
vutikurishanmukha9 <vutikurishanmukha@gmail.com>
2025-07-15 13:37:02 +0000

    week test

--- Weekly Tests/01.Week Test.py
+++ Weekly Tests/01.Week Test.py
@@ -6,3 +6,29 @@ n = input("Enter the date of birthday as dd-mm-yyyy: ")
 print(len(n))
 print(f'{n[6:10]}/{n[3:5]}/{n[0:2]}')
 
+#The even Odd Game 
+#Input:an integer n 
+#Output: "Even Winner" or "Odd winner"
+
+n = int(input("Enter the Number: "))
+s = n % 2
+if s == 0:
+    print("Even Winner")
+else:
+    print("Odd Winner")
+
+#Vowel Replacer Bot 
+#Input: a string msgg
+#Output: Modified string with vowels replaced by *
+
+n = input("Enter the message: ")
+
+s = n.replace('a', '*')
+s = s.replace('e', '*')
+s = s.replace('i', '*')
+s = s.replace('o', '*')
+s = s.replace('u', '*')
+
+print(s)
+
+
