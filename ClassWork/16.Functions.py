2587ac86a95a8f68453953b5b002598ceb6659d4
unknown <vutikurishanmukha@gmail.com>
2025-07-22 12:17:53 +0530

    Functions

--- ClassWork/16.Functions.py
+++ ClassWork/16.Functions.py
@@ -13,3 +13,45 @@ Features of Functions in Python
 5. Encapsulation - Hides implementation details from users.
 6. Parameterization - Functions accept arguments to perform operations dynamically.
 7. Return Values - Functions can return values for further use.'''
+
+data = {'current_balance':12345,'history':[]}
+
+def check_balance():
+    print(f'Current Balance: {data['current_balance']}')
+
+def deposit():
+    amount = int(input("Enter the amount: "))
+    data['current_balance'] += amount
+    data['history'].append(f'Deposited - Rs{amount}')
+    print(f'Deposited - Rs{amount}')
+def withdraw():
+    amount = int(input("Enter the amount: "))
+    if data['current_balance'] >=amount:
+        data['current_balance']-= amount
+        data['history'].append(f'Withdrawed - Rs{'amount'}')
+def check_history():
+    print("Transactions")
+    for i in data['history']:
+        print(i)
+while True:
+    print("======Welcome to ATM======")
+    print('1.Check Balance')
+    print('2.Deposit')
+    print('3.Withdraw')
+    print('4.History')
+    print('0.Exit')
+
+    choice = int(input("Enter your choice:  "))
+    if choice == 0:
+        print('Thanks Visit Again'.center(50,'*'))
+        break
+    elif choice == 1:
+        check_balance()
+    elif choice == 2:
+        deposit()
+    elif choice == 3:
+        withdraw()
+    elif choice == 4:
+        check_history()
+    else:
+        print("Invalid coice. Try Again")
