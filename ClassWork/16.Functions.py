971f8057963eb13d8b625d0acc7afb22bea6ff45
unknown <vutikurishanmukha@gmail.com>
2025-07-22 11:22:24 +0530

    Functions

---
+++ ClassWork/16.Functions.py
@@ -0,0 +1,15 @@
+'''Functions
+
+Definition of Functions
+
+A function in Python is a reusable block of code that performs a specific task. It helps in breaking 
+down large programs into smaller, manageable parts. Functions improve code reusability, readability, and modularity.
+
+Features of Functions in Python
+1. Code Reusability - Write once, use multiple times.
+2. Modularity - Divide code into logical sections.
+3. Maintainability - Easier to debug and modify.
+4. Scalability - Helps in handling complex applications.
+5. Encapsulation - Hides implementation details from users.
+6. Parameterization - Functions accept arguments to perform operations dynamically.
+7. Return Values - Functions can return values for further use.'''
